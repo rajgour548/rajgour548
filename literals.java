f473242ae47e11454a9ef35d3d2db98d83b0e26c
raj gour <gourraj548@gmail.com>
2024-05-09 02:40:11 +0530

    saving changes

---
+++ literals.java
@@ -0,0 +1,18 @@
+public class literals  {
+    public static void main(String args[]){
+        //literals 
+        //for binary number we can use 0b in integer data type variable 
+        int n=0b101;
+        System.out.println(n);
+        int hexavalue=0x7Eb4;
+        System.out.println(hexavalue);
+        int num=10_00_00_000;
+        System.out.println(num);
+        double number=2e10;
+        System.out.println(number);
+        //string literal
+        char c='a';
+        c++;
+        System.out.println(c);
+    }
+}
