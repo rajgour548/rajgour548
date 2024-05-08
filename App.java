306f62782e0790965379a779707bf96d6849f633
raj gour <gourraj548@gmail.com>
2024-05-09 02:06:17 +0530

    data types

--- App.java
+++ App.java
@@ -1,5 +1,11 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)   {
+        // this for single line comment
+        /*this for multiple line comment in java */
+        int a=10;
+        int b=20;
+        int c;
+        c=a+b;
+        System.out.println("this is my first java program and output of c is:"+c);
     }
 }
---
+++ datatypes.java
@@ -0,0 +1,20 @@
+public class datatypes {
+    public static void main(String args[]){
+        //data types
+        //1. integer
+        int num = 10;
+        long n2 = 10l;
+        short n3 = 20;
+        byte n4 = 100;
+       //2. float
+       //double is the default value
+       double n5=1.6;
+       float n6=1.8f;
+       //3. character
+       // follows unicode in java
+       char c='r';
+       //4. boolean
+       //boolean in java doesn,t store 0 or 1 it only stores true or false and note that true or false in java is a keyword
+       boolean b=true;
+    }
+}
