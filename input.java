e24741872b221357bb243fa6ec75640a78f7affa
raj gour <gourraj548@gmail.com>
2024-05-17 00:32:52 +0530

    few changes

---
+++ input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class input {
+    public static void main(String []args){
+        int n;
+    Scanner Sc = new Scanner(System.in);
+    n=Sc.nextInt();// for taking integer as input
+    System.out.println(n);
+    System.out.println(Sc.next());// for taking single string as input
+    System.out.println(Sc.nextLine());//for taking multiple string as input
+    }
+}
---
+++ output.java
@@ -0,0 +1,9 @@
+public class output {
+    public static void main(String[] args){
+        //output demonstration in java
+        int n=10;
+        System.out.print(n);
+        System.out.println("hello world");
+        System.out.println("i am raj");
+    }
+}
